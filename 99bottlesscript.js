c9d6443d238be7d8bdafab223628618f34129514
SiHalpin <148987051+SiHalpin@users.noreply.github.com>
2023-11-22 16:38:24 +0000

    Add files via upload

---
+++ 99bottlesscript.js
@@ -0,0 +1,11 @@
+function bottlesSong(){
+
+var beersOnWall = 99;
+
+var output = document.getElementById("printSong");
+
+while(beersOnWall > 0){
+printSong.innerHTML = printSong.innerHTML + beersOnWall + " bottles of beer on the wall " + beersOnWall + " bottles of beer<br>Take one down, pass it around " + (beersOnWall-1) + " bottles of beer on the wall";
+n--;
+} 
+}
